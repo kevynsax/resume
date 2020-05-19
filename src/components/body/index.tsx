185ddb721e952f5ad32f3ebd0d4a3a04e4f2e5c2
Kevyn Klava <kevynsax@gmail.com>
2020-05-18 23:55:58 -0300

    Deleted avatar

--- src/components/body/index.tsx
+++ src/components/body/index.tsx
@@ -1,14 +1,12 @@
 import React, {Component} from "react";
 import Cover from "../cover";
 import "./body.scss";
-import Avatar from "../avatar";
 import About from "../about";
 
 export default class Body extends Component{
     render = () => {
         return (<div className="body">
             <Cover />
-            <Avatar />
             <About />
         </div>);
     };
