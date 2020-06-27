ef85a88ea05c9312a7e325fffaa01aa83ba20881
Kevyn Klava <kevynsax@gmail.com>
2020-06-27 18:20:45 -0300

    create SEO tags for search engines

---
+++ src/components/utils/share/index.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+// https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fkevynklava.com%2Fblog%2Fhow_to_create_a_docker_for_your_angular_project&amp;src=sdkpreparse
+// https://twitter.com/intent/tweet?text=Hello%20world
+// https://www.linkedin.com/shareArticle?mini=true&url=https://stackoverflow.com/questions/10713542/how-to-make-custom-linkedin-share-button/10737122&title=How%20to%20make%20custom%20linkedin%20share%20button&summary=some%20summary%20if%20you%20want&source=stackoverflow.com
+// https://api.whatsapp.com/send?phone=91XXXXXXXXXX&text=urlencodedtext
+export default class ShareButton extends Component{
+    render = () => {
+        return (
+            <div></div>
+        )
+    }
+}
